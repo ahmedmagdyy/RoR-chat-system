69969fde3841c265384b02ff3ab5a53803cf16c9
Ahmed Magdy <ahmedmagdy2016@gmail.com>
2021-10-06 20:23:53 +0200

    init redis and sidekiq

---
+++ config/initializers/redis.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module RedisCache
+  class << self
+    def redis
+      @redis ||= Redis.new(url: (ENV['REDIS_URL'] || 'redis://127.0.0.1:6379'))
+    end
+  end
+end
